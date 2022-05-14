1e524da563044a4a505f5379d717249056ccdcef
AndrewGalkin <127788127788a@gmail.com>
2022-05-14 13:02:37 +0300

    сделал.

--- src/lesson6/homework/Converting.java
+++ src/lesson6/homework/Converting.java
@@ -1,7 +1,5 @@
 package lesson6.homework;
 
-import lesson6.homework1.Bodyguard;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -11,11 +9,11 @@ public class Converting {
 
 
     public static void main(String[] args) {
-        Scanner INPUTPARAMS = new Scanner(System.in);
+        Scanner inputParams = new Scanner(System.in);
         List<ConvertItem> convertItems = new ArrayList<>();
         String actionMessage ="Choose action: \n1.Choose \n2.Exit,press any key for exit.";
         System.out.println(actionMessage);
-        String input = INPUTPARAMS.nextLine();
+        String input = inputParams.nextLine();
         while("choose".equals(input.toLowerCase(Locale.ROOT).trim())) {
            ConvertItem convertItem = createTemperatureObject();
             if (convertItem != null) {
@@ -23,7 +21,7 @@ public class Converting {
             }
 
             System.out.println(actionMessage);
-            input = INPUTPARAMS.nextLine();
+            input = inputParams.nextLine();
         }
         for (ConvertItem convertItem : convertItems) {
             convertItem.convert();
@@ -31,17 +29,17 @@ public class Converting {
     }
     public static ConvertItem createTemperatureObject() {
         try {
-            Scanner INPUTPARAMS = new Scanner(System.in);
+            Scanner inputParams = new Scanner(System.in);
             System.out.println("Choose the type you want to convert : \n1. From Celsius to Fahrenheit. \n2. From Celsius to Kelvin.");
-            String choice = INPUTPARAMS.nextLine();
+            String choice = inputParams.nextLine();
             switch (choice.toLowerCase(Locale.ROOT).trim()) {
                 case "fahrenheit":
                     System.out.println("Enter degree in Celsius format");
-                    ConvertItem.degree = INPUTPARAMS.nextFloat();
+                    ConvertItem.degree = inputParams.nextFloat();
                     return new Fahrenheit();
                 case "kelvin":
                     System.out.println("Enter degree in Celsius format");
-                    ConvertItem.degree = INPUTPARAMS.nextFloat();
+                    ConvertItem.degree = inputParams.nextFloat();
                     return new Kelvin();
             }
             System.out.println("Incorrect choice type " + choice + ". Try again");
